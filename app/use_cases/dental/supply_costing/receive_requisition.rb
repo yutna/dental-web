74a258fe5af743f3cb721a1635f190f242e29a25
Yutthana Siphuengchai <yutthana.s@outlook.com>
2026-04-11 16:08:14 +0700

    [T05.06] Add receive/cancel transitions with stock-in side effects

---
+++ app/use_cases/dental/supply_costing/receive_requisition.rb
@@ -0,0 +1,31 @@
+module Dental
+  module SupplyCosting
+    class ReceiveRequisition < BaseUseCase
+      def call(requisition:, receiver_id:)
+        movements = []
+
+        ActiveRecord::Base.transaction do
+          requisition.receive!(receiver_id: receiver_id)
+
+          requisition.line_items.each do |line_item|
+            result = PostStockMovement.call(
+              item_type: line_item.item_type,
+              item_code: line_item.item_code,
+              direction: "in",
+              quantity: line_item.quantity,
+              unit: line_item.unit,
+              source: "requisition",
+              reference_type: "requisition",
+              reference_id: requisition.requisition_id,
+              actor_id: receiver_id,
+              note: "Stock-in from requisition #{requisition.requisition_id}"
+            )
+            movements << result[:movement]
+          end
+        end
+
+        { requisition: requisition, movements: movements }
+      end
+    end
+  end
+end
